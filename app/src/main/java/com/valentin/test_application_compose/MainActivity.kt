f7b9d0d8130a8d6d3c78b2bf5148da7f49e56f5c
Valentin <32tut32@gmail.com>
2023-06-27 11:53:01 +0300

    first task

--- app/src/main/java/com/valentin/test_application_compose/MainActivity.kt
+++ app/src/main/java/com/valentin/test_application_compose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.valentin.test_application_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.valentin.test_application_compose.ui.theme.TestapplicationcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,19 +29,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    GreetingText(
+                        message = "Hello, world!",
+                        from = "Valentin"
+                    )
                 }
             }
         }
     }
 }
 
-
+@Composable
+fun GreetingText(modifier: Modifier = Modifier, message: String, from: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     TestapplicationcomposeTheme {
-        
+        GreetingText(message = "Hello, world", from = "Valentin")
     }
 }
