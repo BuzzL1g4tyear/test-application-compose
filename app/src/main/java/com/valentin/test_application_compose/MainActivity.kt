e8b3aaf5634b27094a467bd5b18f9663246da1c7
Valentin <32tut32@gmail.com>
2023-06-27 10:57:03 +0300

    init commit

---
+++ app/src/main/java/com/valentin/test_application_compose/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.valentin.test_application_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.valentin.test_application_compose.ui.theme.TestapplicationcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestapplicationcomposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    TestapplicationcomposeTheme {
+        
+    }
+}
